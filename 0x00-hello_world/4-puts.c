73e8d3b386ac299b0a345213042492926f6787ef
oxtat <wagura.dev@gmail.com>
2023-08-10 13:43:19 -0700

    Modified puts.c to betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
+/*
+ * File: 4-puts.c
+ * Author: Brian Wagura <wagura.dev@gmail.com>
+ */
+
 #include <stdio.h>
 
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
