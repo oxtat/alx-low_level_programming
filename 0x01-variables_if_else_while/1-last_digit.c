2107e56c098c7abfb9546b6253e72878df606a33
oxtat <wagura.dev@gmail.com>
2023-08-12 00:19:16 -0700

    Correct new line error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,8 +20,8 @@ int main(void)
 	if (y > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, y);
 	else if (y == 0)
-		printf("Last digit of %d is %d and is 0", n, y);
+		printf("Last digit of %d is %d and is 0\n", n, y);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, y);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
 	return (0);
 }
