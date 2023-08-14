c8f4ddcec264cd5b365c9a2a0a85ceefee4d6fe2
oxtat <wagura.dev@gmail.com>
2023-08-14 12:16:01 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-
 /*
  * File: 101-quote.c
  * Auth: Brian Wagura <wagura.dev@gmail.com>
@@ -13,12 +12,8 @@
  * Return: Always 1.
  */
 
-
-
-
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 59);
 	return (1);
 }
-
